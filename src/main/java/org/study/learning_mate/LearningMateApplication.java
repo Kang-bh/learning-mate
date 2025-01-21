2d4107a26200a77c7fde70c6a4b5ad8e42cc9f84
Kang-bh <kabyho3513@gmail.com>
2025-01-21 15:16:56 +0900

    Initialize repository

---
+++ src/main/java/org/study/learning_mate/LearningMateApplication.java
@@ -0,0 +1,13 @@
+package org.study.learning_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/study/learning_mate/LearningMateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.study.learning_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
