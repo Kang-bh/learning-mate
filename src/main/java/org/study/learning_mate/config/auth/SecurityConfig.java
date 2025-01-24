ac599b586a9e4ef444345d55667e72f664ddec62
Kang-bh <kabyho3513@gmail.com>
2025-01-25 02:15:26 +0900

    fix

--- src/main/java/org/study/learning_mate/config/auth/SecurityConfig.java
+++ src/main/java/org/study/learning_mate/config/auth/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.study.learning_mate.config.auth;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,12 +13,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.study.learning_mate.CustomOAuth2UserService;
 import org.study.learning_mate.CustomSuccessHandler;
 import org.study.learning_mate.RefreshTokenRepository;
 import org.study.learning_mate.UserRepository;
 import org.study.learning_mate.config.auth.jwt.JWTFilter;
 import org.study.learning_mate.config.auth.jwt.JWTUtil;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -56,6 +61,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, UserRepository userRepository) throws Exception {
+
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })));
 
         http
                 .csrf((auth) -> auth.disable());
