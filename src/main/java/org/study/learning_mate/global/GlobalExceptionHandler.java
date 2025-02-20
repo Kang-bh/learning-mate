74938568620e7de2686d89e4702c5c4c6bc6001d
Kang-bh <kabyho3513@gmail.com>
2025-02-20 13:53:39 +0900

    [FIX] resolve conflict version

--- src/main/java/org/study/learning_mate/global/GlobalExceptionHandler.java
+++ src/main/java/org/study/learning_mate/global/GlobalExceptionHandler.java
@@ -4,10 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.study.learning_mate.global.ErrorResponse;
 
 @Slf4j
-@ControllerAdvice
+//@ControllerAdvice
+@RestControllerAdvice(annotations = {RestController.class}, basePackages = {"org.study.learning_mate.controller"})
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ErrorResponse.class)
